871af1660de40aec5c6ff200e5886054f20ab9c3
Queen-01 <quienzy.ongeye@student.moringaschool.com>
2021-04-10 15:12:18 +0300

    add js file

---
+++ app.js
@@ -0,0 +1,8 @@
+var century, year, month, date;
+// get the input
+function getInput() {
+    century = parseInt(document.getElementById("century").value);
+    year = parseInt(document.getElementById("year").value);
+    month = parseInt(document.getElementById("month").value);
+    date = parseInt(document.getElementById("date").value);
+}
