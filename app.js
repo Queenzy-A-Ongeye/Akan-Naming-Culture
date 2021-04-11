8d0ad2f2a099ca75b1fae0ef3aca8bd82179dad5
Queen-01 <quienzy.ongeye@student.moringaschool.com>
2021-04-11 10:55:38 +0300

    add changes made on html file

--- app.js
+++ app.js
@@ -6,21 +6,38 @@ function getInput() {
     month = parseInt(document.getElementById("month").value);
     dayofthemonth = parseInt(document.getElementById("monthday").value);
 }
-if ((century == "") && (century <= 0) && (century >= 30)){
-    if ((century=="")){
+if ((century == "") && (century <= 0) && (century >= 30)) {
+    if ((century == "")) {
         alert("Please put in the correct gender");
-    }else if ((year == "") && (year <= 0) && (year > 1900)){
+    } else if ((year == "") && (year <= 0) && (year > 1900)) {
         return false;
-    }else if((year == "")){
+    } else if ((year == "")) {
         alert("Please enter the correct year");
-    }else if ((month == "") && (month > 12) && (month <=0)){
+    } else if ((month == "") && (month > 12) && (month <= 0)) {
         return false;
-    }else if ((month == "")){
+    } else if ((month == "")) {
         alert("Please enter the correct month");
-    }else if ((dayofthemonth == "") && (dayofthemonth > 31) && (dayofthemonth <= 0)){
+    } else if ((dayofthemonth == "") && (dayofthemonth > 31) && (dayofthemonth <= 0)) {
         return false;
-    }else if ((dayofthemonth == "")){
+    } else if ((dayofthemonth == "")) {
         alert("Please enter the correct date");
         return false;
     }
+}
+//Calculating function
+function calculateDay() {
+    getInput();
+    dayOfWeek = ((((century / 4) - 2 * century - 1) + ((5 * year / 4)) + ((26 * (month + 1) / 10)) + dayOfMonth) % 7) - 1;
+    console.log("dayoftheweek");
+    return (Math.floor(dayOfWeek));
+    if (dayOfWeek < 0) {
+        dayoftheweek = dayOfWeek * -1
+    } else if (dayoftheweek > 0) {
+        return dayoftheweek;
+    }
+}
+function checkDayOfWeek(){
+    day = calculateDay()
+    checkGender;
+    console.log("the function runs")
 }
